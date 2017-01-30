0c71f77e96040f0bae18cb2c27fd305b5e3ba9dd
WebsoftProfession <dotsquares@deepaksharma2.local>
2017-01-30 14:28:40 +0530

    Initial Commit

---
+++ Recording Animation LayerTests/Recording_Animation_LayerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Recording_Animation_LayerTests.swift
+//  Recording Animation LayerTests
+//
+//  Created by Dotsquares on 1/30/17.
+//  Copyright © 2017 Dotsquares. All rights reserved.
+//
+
+import XCTest
+@testable import Recording_Animation_Layer
+
+class Recording_Animation_LayerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
